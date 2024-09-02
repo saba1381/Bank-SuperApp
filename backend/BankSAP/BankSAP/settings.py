ee95ba13e99ad923bb5024208786358bc6fd7b31
Saba Basiri <basirisaba81@gmail.com>
2024-09-02 22:03:22 +0330

    I connect django to MySql

--- backend/BankSAP/BankSAP/settings.py
+++ backend/BankSAP/BankSAP/settings.py
@@ -101,8 +101,12 @@ WSGI_APPLICATION = 'BankSAP.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bank-db', 
+        'USER': 'root', 
+        'PASSWORD': 'Mary13813929',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306', 
     }
 }
 
