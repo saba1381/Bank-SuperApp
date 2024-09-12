e4b903bf18745a81e1982fb5886ca2027c4bab83
Saba Basiri <basirisaba81@gmail.com>
2024-09-12 19:23:59 +0330

    backend for Edit Profile

--- backend/BankSAP/BankSAP/settings.py
+++ backend/BankSAP/BankSAP/settings.py
@@ -53,7 +53,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
-    'REFRESH_TOKEN_LIFETIME': timedelta(hours=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(hours=10),
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
