9ab5efb5ddfb3afc46d4f9529cc34cd8132ddc80
Saba Basiri <basirisaba81@gmail.com>
2024-10-30 10:44:17 +0330

    Add API for sending pooya for Charging

--- backend/BankSAP/charge/urls.py
+++ backend/BankSAP/charge/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import RechargeAPIView
+from .views import RechargeAPIView , SendOTPAPIView , VerifyCardAndOTPAPIView
 
 urlpatterns = [
     path('recharge/', RechargeAPIView.as_view(), name='recharge_mobile'),
+    path('send-otp/', SendOTPAPIView.as_view(), name='send-otp'),
+    path('verify-info/', VerifyCardAndOTPAPIView.as_view(), name='verify-info'),
 ]
--- backend/BankSAP/charge/views.py
+++ backend/BankSAP/charge/views.py
@@ -5,6 +5,10 @@ from django.core.cache import cache
 from .models import Recharge
 from .serializers import RechargeSerializer
 from rest_framework.permissions import IsAuthenticated
+import random
+import pyotp
+import base64
+import secrets
 
 class RechargeAPIView(APIView):
     permission_classes = [IsAuthenticated]
@@ -24,6 +28,55 @@ class RechargeAPIView(APIView):
         
 
         recharge_data = {'mobile_number': mobile_number, 'amount': amount}
-        cache.set(f'recharge_{request.user.id}', recharge_data, timeout=600)
+        cache.set(f'recharge_{request.user.id}', recharge_data, timeout=240)
 
         return Response({"detail": "اطلاعات شارژ با موفقیت ذخیره شد."}, status=status.HTTP_200_OK)
+    
+
+class SendOTPAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        recharge_data = cache.get(f'recharge_{request.user.id}')
+        if not recharge_data:
+            return Response({"detail": "اطلاعات شارژ یافت نشد. ابتدا اطلاعات شارژ را ثبت کنید."}, status=status.HTTP_400_BAD_REQUEST)
+        totp = pyotp.TOTP('base32secret3232', digits=5)
+        otp_code = totp.now()
+
+        cache.set(f'otp_{request.user.id}', otp_code, timeout=120)
+        phone_number = request.user.phone_number
+        print(f"send code to this number {phone_number} is : {otp_code}")
+        return Response({"detail": "کد OTP ارسال شد."}, status=status.HTTP_200_OK)
+
+
+class VerifyCardAndOTPAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+
+        card_number = request.data.get('card_number')
+        cvv2 = request.data.get('cvv2')
+        cardMonth = request.data.get('cardMonth')
+        cardYear = request.data.get('cardYear')
+        dynamicPassword = request.data.get('dynamicPassword')
+
+
+        cached_otp = cache.get(f'otp_{request.user.id}')
+        if not cached_otp or cached_otp != dynamicPassword:
+            return Response({"detail": "کد OTP نامعتبر است یا منقضی شده است."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        recharge_data = cache.get(f'recharge_{request.user.id}')
+        if not recharge_data:
+            return Response({"detail": "اطلاعات شارژ یافت نشد. لطفا اطلاعات را دوباره وارد کنید."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        recharge = Recharge(
+            user=request.user,
+            mobile_number=recharge_data['mobile_number'],
+            amount=recharge_data['amount']
+        )
+        recharge.save()
+        cache.delete(f'recharge_{request.user.id}')
+        cache.delete(f'otp_{request.user.id}')
+
+
+        return Response({"detail": "اطلاعات کارت و کد OTP با موفقیت تأیید شد."}, status=status.HTTP_200_OK)
